005858c1a3cca3c0bda904ff55fd0ffada195542
thanakrit <thnankrit.w@ku.th>
2024-03-08 13:34:20 +0700

    fix upload

--- src/components/pageStaff/roomList-page/roomListPage.tsx
+++ src/components/pageStaff/roomList-page/roomListPage.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect ,useState} from 'react';
-import { Button, Space, Modal, Upload  } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from 'react';
+import { Button, Space, Modal, Upload, message, Image } from 'antd';
+import { PlusOutlined, UploadOutlined } from '@ant-design/icons';
 import RoomListTable from './roomListTable';
 import RoomService from './../../../services/room.service';
 import type { GetProp, UploadFile, UploadProps } from 'antd';
 import { useParams } from 'react-router-dom';
 
 interface Room {
-  // key: number;
   id: number;
   floor_name: string;
   room_name: string;
@@ -25,52 +24,30 @@ interface RoomData {
   roomStatus: string;
 }
 
-type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
-
-const getBase64 = (file: FileType): Promise<string> =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = (error) => reject(error);
-  });
-
-
-
-
 const RoomListPage: React.FC = () => {
   const [rooms, setRooms] = React.useState<Room[]>([]);
   const [roomsData, setRoomsData] = React.useState<RoomData[]>([]);
   const { apartId } = useParams()
-
-  const [previewOpen, setPreviewOpen] = useState(false);
-  const [previewImage, setPreviewImage] = useState('');
-  const [previewTitle, setPreviewTitle] = useState('');
   const [fileList, setFileList] = useState<UploadFile[]>([]);
-   
 
-  const handleCancel = () => setPreviewOpen(false);
-
-  const handlePreview = async (file: UploadFile) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj as FileType);
-    }
-
-    setPreviewImage(file.url || (file.preview as string));
-    setPreviewOpen(true);
-    setPreviewTitle(file.name || file.url!.substring(file.url!.lastIndexOf('/') + 1));
+  const props: UploadProps = {
+    name: 'file',
+    action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+        setFileList([info.file]);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
   };
-
-  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
-    setFileList(newFileList);
-
-  const uploadButton = (
-    <button style={{ border: 0, background: 'none' }} type="button">
-      <PlusOutlined />
-      <div style={{ marginTop: 8 }}>Upload</div>
-    </button>
-  );
-  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -87,29 +64,15 @@ const RoomListPage: React.FC = () => {
   return (
     <>
       <div>
-        <p className =' mb-5 text-lg font-bold'>
-            เพิ่มรูปภาพผังห้องพัก
-        </p>
-      <div className= "mb-5">
-      <Upload
-          action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-          listType="picture-card"
-          fileList={fileList}
-          onPreview={handlePreview}
-          onChange={handleChange}
-      >
-        {fileList.length >= 8 ? null : uploadButton}
-      </Upload>
-      <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
-        <img alt="example" style={{ width: '100%' }} src={previewImage} />
-      </Modal>
-      </div>
-        {/* <Space style={{ marginBottom: 16 }}>
-          <Button type="primary" icon={<PlusOutlined />}>เพิ่มรูปภาพผังห้องพัก</Button>
-        </Space> */}
-        <RoomListTable data={roomsData}/>
-      </div>
+        <div className="mb-5">
+        <Upload {...props} showUploadList={false}>
+          <Button icon={<UploadOutlined />}>เพิ่มรูปภาพผังห้องพัก</Button>
+        </Upload>
 
+          {fileList.length > 0 && <Image src={fileList[0].thumbUrl} />}
+        </div>
+        <RoomListTable data={roomsData} />
+      </div>
     </>
   );
 };
